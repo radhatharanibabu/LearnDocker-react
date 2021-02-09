3602d8733a44e98f2361d33cea0558e26305bb45
radhtharanibabu <radha.m.tharani@gmail.com>
2021-02-09 22:04:35 +0000

    test fixed

--- src/App.test.js
+++ src/App.test.js
@@ -2,21 +2,6 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hellow/i);
-  expect(linkElement).toBeInTheDocument();
+heDocument();
 });
 
-test('renders learn react link2', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hellow/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-
-
-test('renders learn react link3', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hellow/i);
-  expect(linkElement).toBeInTheDocument();
-});
