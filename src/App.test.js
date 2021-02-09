3e5e5e097da8449c32e64bc869d003f1e7112e6c
radhtharanibabu <radha.m.tharani@gmail.com>
2021-02-09 20:30:20 +0000

    initial code commit

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,17 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders learn react link2', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Hellow/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+
+test('renders learn react link3', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Hellow/i);
+  expect(linkElement).toBeInTheDocument();
+});
